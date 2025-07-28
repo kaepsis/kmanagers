1afc0c45aaa7a446106a4b523aa58865ccf7a0f8
kaepsis <kaepsis@users.noreply.github.com>
2025-07-28 08:17:53 +0000

    deploy: ba0ff1d351ba810880c089f79a33776ebea37784

---
+++ type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.github.kaepsis","l":"Chat"},{"p":"com.github.kaepsis","l":"ItemFactory"},{"p":"com.github.kaepsis","l":"Time"}];updateSearchResults();
