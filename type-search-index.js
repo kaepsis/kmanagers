4d5adbf26dcb5e6d402ffc1733feecf802edb872
Kaepsis <durastante.lorenzo02@gmail.com>
2025-07-28 10:05:28 +0200

    removed files

--- type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.github.kaepsis","l":"Chat"},{"p":"com.github.kaepsis","l":"ItemFactory"},{"p":"com.github.kaepsis","l":"Time"}];updateSearchResults();
